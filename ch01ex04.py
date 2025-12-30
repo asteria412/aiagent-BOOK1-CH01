d3aa893e5348ac067f8ded67209d9fe9957245cb
asteria412 <asteria412@naver.com>
2025-12-30 11:56:15 +0900

    Numpy 간단예제

---
+++ ch01ex04.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import matplotlib.patches as plt
+
+# print("np.__version__", np.__version__)
+# print("pd.__version__", pd.__version__)
+# print("OK")
+
+num_list = [1,2,3,4,5]
+print(num_list)
+data = np.array(num_list) #넘파이 array > 여러가지 집계 함수가 포함되어있다
+print(data)
+
+print(data.mean())
+print(data.max())
